96fa72e615142f7bab9d20e16f09e332cc7ad033
umeshtiwari233 <umesh.tiwari233@gmail.com>
2023-10-29 16:27:46 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <View>
+          <Text>hi </Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
